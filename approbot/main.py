7dc82cb62e597ba1e6343f10ef5642cf70fc7ac5
lubinRld <lubin.roelandt@gmail.com>
2026-05-19 11:09:36 +0200

    reset robot position before steps and add move_time parameter

--- approbot/main.py
+++ approbot/main.py
@@ -2,28 +2,20 @@ import robot_controller
 
 def main():
 
-    ip_adress_1 = "192.168.1.1"
-    ip_address_2 = "192.168.1.2"
+    ip_adress_1 = "192.168.0.103"
 
     my_robot = robot_controller.RobotController()
-    my_second_robot = robot_controller.RobotController()
 
     my_robot.connect(ip_adress_1)
-    my_second_robot.connect(ip_address_2)
-    
-
-    my_robot._move_arm("left", 100)
-    my_robot._move_arm("right", -100)
-    my_robot._step("forward", 2)
-    my_robot._step("left", 1)
-    my_robot._eye_expression("angry")
 
     my_robot.execute_action("U",2)
-    my_robot.execute_action("L",2)
     my_robot.execute_action("ARU")
     my_robot.execute_action("ALB")
     my_robot.execute_action("XNG")
-
+    my_robot.execute_action("L",2)
+    my_robot.execute_action("ARB")
+    my_robot.execute_action("ALU")
+    my_robot.execute_action("XNT")
 
 
     my_robot.disconnect()
--- approbot/robot_controller.py
+++ approbot/robot_controller.py
@@ -11,64 +11,64 @@ class RobotController:
             self.robot=Marty("wifi", ip_address)
             self.is_connected=True
             print("The robot is connected")
+            print(Marty.JOINT_IDS)
         except Exception as e:
             print(f"Connection failed : {e}")
             self.is_connected=False
     
-    def execute_action(self, actionCode, nbrAction):
-        if(self.is_connected==False | self.robot==None):
+    def execute_action(self, actionCode, nbrAction=None):
+        if self.is_connected==False or self.robot is None:
             print(f"The robot is not connected, you can't do the movement code : {actionCode}")
             return False
-        if(actionCode == "U"):
+        if actionCode == "U":
+            self._reset_position()
             self._step("forward", nbrAction)
-        elif(actionCode == "B"):
+        elif actionCode == "B":
+            self._reset_position()
             self._step("backward", nbrAction)
-        elif(actionCode == "L"):
+        elif actionCode == "L":
+            self._reset_position()
             self._step("left", nbrAction)
-        elif(actionCode == "R"):
+        elif actionCode == "R":
+            self._reset_position()
             self._step("right", nbrAction)
-        elif(actionCode == "ARU"):
+        elif actionCode == "ARU":
             self._move_arm("right", 100)
-        elif(actionCode == "ARB"):
+        elif actionCode == "ARB":
             self._move_arm("right", -100)
-        elif(actionCode == "ALU"):
+        elif actionCode == "ALU":
             self._move_arm("left", 100)
-        elif(actionCode == "ALB"):
+        elif actionCode == "ALB":
             self._move_arm("left", -100)
-        #elif(actionCode == "XSD"): looks like there is no sad eyes, TO CHECK
-            #self._eye_expression("sad")
-        elif(actionCode == "XNG"):
+        elif actionCode == "XNG":
             self._eye_expression("angry")
-        elif(actionCode == "XNT"):
+        elif actionCode == "XNT":
             self._eye_expression("normal")
         else:
             print(f"the action code you filled : {actionCode} doesn't exist")
             return False
 
-
-
-
     def disconnect(self):
-        if(self.is_connected and self.robot != None):
+        if self.is_connected and self.robot != None:
             self.robot.close()
             self.is_connected=False
             print("The robot is disconnected")
 
-    def _move_arm(self, side, angle):
+    def _move_arm(self, side, angle, _move_time=1500):
         if side == "left":
-            self.robot.arms(angle, 0, 500)
+            self.robot.move_joint("left arm",angle, _move_time)
         elif side == "right":
-            self.robot.arms(0, angle, 500)
+            self.robot.move_joint("right arm", angle, _move_time)
         else:
             print(f"move_arm: the only direction known is left or right, or, you indicated {side}")
         
-    def _step(self, direction, step_number):
+    def _step(self, direction, step_number, _move_time=1500):
         if direction == "forward":
-            self.robot.walk(step_number)
+            self.robot.walk(step_number, move_time=_move_time)
         elif direction == "backward":
-            self.robot.walk(step_number, step_length=-25)
+            self.robot.walk(step_number, step_length=-25, move_time=_move_time)
         elif direction == "left" or direction == "right":
-            self.robot.sidestep(direction,step_number)
+            self.robot.sidestep(direction,step_number,move_time= _move_time)
         else:
             print(f"step: the only direction known is forward, backward, left or right or, you indicated {direction}")
 
@@ -79,4 +79,7 @@ class RobotController:
         else:
             print(f"eye_expression: the only valid expressions are {valid_emotion} or you indicated {emotion}")
         
+    def _reset_position(self):
+        self.robot.stand_straight()
+
 
