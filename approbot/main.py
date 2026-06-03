9c1e655c2ec35e4ae47b7f2da848b37a70625b31
lubinRld <lubin.roelandt@gmail.com>
2026-06-03 11:11:16 +0200

    play game method tested and works

--- approbot/main.py
+++ approbot/main.py
@@ -6,10 +6,10 @@ def main():
 
     my__client = robot_client.RobotClient()
     my_robot = robot_controller.RobotController()
-    my_reader = read_dance.ReadDance("example.dance")
+    my_reader = read_dance.ReadDance("approbot/example.dance")
 
     my_manager = game_manager.GameManager(my_robot,my__client,my_reader)
-    my_manager.prepare_game("192.168.1.1")
+    my_manager.prepare_game("192.168.0.103")
     my_manager.play_game()
     my_manager.end_game()
     
