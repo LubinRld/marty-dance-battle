6de3e2ce55d6a391703c981693dbb5df519546fd
lubinRld <lubin.roelandt@gmail.com>
2026-06-08 19:31:51 +0200

    Added an Icon for the window

--- approbot/config.py
+++ approbot/config.py
@@ -2,6 +2,7 @@ WINDOW_TITLE = "MartyApp"
 BACKGROUND_STYLE = "background-color: #98d7d6;"
 WINDOW_WIDTH = 440
 WINDOW_HEIGHT = 500
+WINDOW_ICON_PATH = "approbot/Logo.png"
 
 DEFAULT_COLOR_REFERENCES = {
     "Black" : (25,12,11), 
